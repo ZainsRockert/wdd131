b10be2defb28ef218a1e4e4764b9eb4b3ff759b3
ZainsRockert <marcosjared02@gmail.com>
2024-11-16 23:20:40 -0400

    W02-Picture album

---
+++ week02/scripts/bom.js
@@ -0,0 +1,21 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");//you need to fill in the blank to reference the HTML element that is a unordered list element.
+
+button.addEventListener("click", function () {
+
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        input.value = "";
+        input.focus();
+    }
+});
